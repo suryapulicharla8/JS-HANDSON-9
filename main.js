0f113e8badc949030741ee78cc32ef863ebe2a1e
suryapulicharla8 <suryapulicharla8@gmail.com>
2022-12-14 09:44:37 +0530

    second commit

--- main.js
+++ main.js
@@ -15,16 +15,37 @@
  console.log(className)
 
 // 4 Question 
-function onbuttonclick(){
-    var flex=document.getElementByClassName('flex-container')
-    if(flex.style.flexDirection == "row"){
-        flex.style.flexDirection="column"
-        console.log('column')
+
+// let dir=document.querySelector('#dir');
+// dir.addEventListener('click',function(){
+//     let direction=document.querySelector('.mainFlex');
+//     if(direction.style.flexDirection == 'column'){
+//         direction.style.flexDirection='row'
+//     }else{
+//         direction.style.flexDirection='column';
+//     }
+// })
+let button=document.querySelector('#click');
+button.addEventListener('click',function(){
+    let change=document.querySelector('.flex-box');
+    if(change.style.flexDirection=="column"){
+        change.style.flexDirection='row'
+        change.style.paddingLeft="250px"
     }else{
-        flex.style.flexDirection='row'
-        console.log('row')
+        change.style.flexDirection="column"
     }
-}
+})
+
+// function onbuttonclick(){
+//     let flex=document.getElementByClassName('button').addEventListener('click')
+//     if(flex.style.flexDirection == "column"){
+//         flex.style.flexDirection="row"
+//         console.log('row')
+//     }else{
+//         flex.style.flexDirection='column'
+//         console.log('column')
+//     }
+// }
 
 // question 5
 let head=document.querySelector('h3').innerHTML="hello are you fine?"
@@ -32,19 +53,42 @@ let head=document.querySelector('h3').innerHTML="hello are you fine?"
 // 6 question
 
 // Question 7
-// let header=document.querySelector('#header')
-// header.style.color="red"
-// header.style.backgroundColor="black"
+let header=document.querySelector('#header')
+header.style.color="red"
+header.style.backgroundColor="black"
 
 let heading=document.querySelector('.heading')
 heading.style.color="gold"
 heading.style.backgroundColor="black"
 
 // 8
-
+document.querySelector('#changeTxt').addEventListener('click',function(){
+    document.querySelector('#chngtxt').innerHTML="Elevation Accadamy"
+})
 
 // 9
 
 // 10
+function  value(){
+    let sel=document.querySelector('#years')
+    let msg=sel.options[sel.selectedIndex].text;
+    document.querySelector('#selValue').innerHTML=msg
+}
+document.querySelector('#selBtn').addEventListener('click',function(){
+    value();
+})
 
 // 11
+
+// 12 q
+function clock(){
+    let time=new Date();
+    let hourr=time.getHours();
+    let minute=time.getMinutes();
+    let second=time.getSeconds();
+    
+    document.getElementById("hr").innerHTML=hourr + ":"
+    document.getElementById('min').innerHTML=minute + ":"
+    document.getElementById('sec').innerHTML=second + ":"
+}
+setInterval(clock,1000);
